91fa70eb7998b1510ba3a3292407b40fe79dfb42
Cara Heacock <caraheacock@gmail.com>
2016-05-29 02:21:04 -0500

    Refactors clock so that it no longer needs javascript to fill the window

--- js/main.js
+++ js/main.js
@@ -5,34 +5,10 @@ $('document').ready(function(){
         hours = now.getHours(),
         $window = $(window),
         $body = $('body'),
-        $homePageNav = $('.home #nav'),
-        $homePageClock = $('.home #clock-container'),
         $clockFace = $('#clock-face'),
         $secondHand = $('#second-hand'),
         $minuteHand = $('#minute-hand'),
         $hourHand = $('#hour-hand');
-        
-    // Positions main nav and resizes clock according to page
-    var positionAndResize = function() {
-        var windowHeight = $window.height(),
-            windowWidth = $window.width(),
-            mainPageNavHeight = $homePageNav.height() + 40,
-            minimum = 600;
-        
-        if (windowHeight <= minimum) {
-            $homePageNav.css('top', minimum/2 - mainPageNavHeight);
-            $homePageClock.css({
-                'padding-bottom':   minimum*0.9,
-                'width':            minimum*0.9
-            });
-        } else {
-            $homePageNav.css('top', windowHeight/2 - mainPageNavHeight);
-            $homePageClock.css({
-                'padding-bottom':   windowHeight*0.9,
-                'width':            windowHeight*0.9
-            });
-        }
-    }
     
     // Loads color schemes depending on the hour of the day
     var loadColors = function() {
@@ -41,7 +17,7 @@ $('document').ready(function(){
         }
         
         for (i = 0; i < 24; i++) {
-            if (i != hours && $body.hasClass('hour' + i)) {
+            if (i !== hours && $body.hasClass('hour' + i)) {
                 $body.removeClass('hour' + i);
             }
         }
@@ -63,15 +39,14 @@ $('document').ready(function(){
         loadColors();
     }
     
-    // Main Flow
-    positionAndResize(); // Sets position when page is first loaded
-    $window.resize(positionAndResize); // Resets position whenever window size is changed
     tick();
-    loadColors();
+    setInterval(tick, 1000);
+    
+    // The "prevent-transition" class on the body prevents flashing colors
+    // when the theme is added on page load.
     setTimeout(function() {
         $body.removeClass('prevent-transition');
     }, 1);
-    setInterval(tick, 1000);
     
     // Back to top button functionality
     var $backToTop = $('.back-to-top');
